54ae3e9aa33137ef4cf7fe5cf39d62545ae33c98
asidd <asidd@users.noreply.github.com>
2024-04-27 14:46:24 -0500

    Update install.sh for linux and macOS
    Script has been updated to install both linux an dmacOS builds

--- surveilr/install.sh
+++ surveilr/install.sh
@@ -1,4 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+# This script will install only Linux and MacOS builds
+
+# Detect the platform
+PLATFORM=$(uname -s | tr '[:upper:]' '[:lower:]')
+
+# select the BUILD
+case "$PLATFORM" in
+    "darwin") BUILD="apple-darwin.zip" ;;
+    "linux") BUILD="unknown-linux-musl.tar.gz" ;;
+    *) echo "Unsupported platform"; exit 1 ;;
+esac
 
 # Set the SURVEILR_HOME environment variable to the current directory if not already set
 : ${SURVEILR_HOME:=$(pwd)}
@@ -11,9 +23,19 @@ REPO_NAME="releases.opsfolio.com"
 API_URL="https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases/latest"
 
 # Fetch the latest release tag from GitHub API and construct the download URL
-DOWNLOAD_URL=$(curl -s $API_URL | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | xargs -I {} echo "https://github.com/$REPO_OWNER/$REPO_NAME/releases/download/{}/resource-surveillance_{}_x86_64-unknown-linux-musl.tar.gz")
+DOWNLOAD_URL=$(curl -s $API_URL | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | xargs -I {} echo "https://github.com/$REPO_OWNER/$REPO_NAME/releases/download/{}/resource-surveillance_{}_x86_64-${BUILD}")
 
 # Download and extract the binary to the SURVEILR_HOME directory
-curl -sL $DOWNLOAD_URL | tar -xz -C $SURVEILR_HOME
+if [ "$PLATFORM" = "darwin" ]; then
+	curl -sL $DOWNLOAD_URL -o temp.zip && unzip -j -q temp.zip -d $SURVEILR_HOME && rm temp.zip
+
+else
+  if [[ "$BUILD" =~ tar\.gz$ ]]; then
+    curl -sL $DOWNLOAD_URL | tar -xz -C $SURVEILR_HOME
+  else
+    echo "Unsupported archive format: $BUILD"
+    exit 1
+  fi
+fi
 
 echo "Download and extraction complete. Binary is in $SURVEILR_HOME"
