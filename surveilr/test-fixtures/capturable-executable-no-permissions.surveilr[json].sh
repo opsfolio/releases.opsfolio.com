6e29e04c7437a405d7943dea8e0d30d1f22d2f9b
Baasit <baasisek01@gmail.com>
2024-09-26 12:45:27 +0100

    chore(app): removed test fixtures

--- surveilr/test-fixtures/capturable-executable-no-permissions.surveilr[json].sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-# echo this file does not have its executable bit set so it will not be executable
--- surveilr/test-fixtures/echo-stdin.surveilr[json].sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Read from STDIN and echo it so that it gets stored in the database;
-# This is useful for testing the capturable executable context.
-cat
